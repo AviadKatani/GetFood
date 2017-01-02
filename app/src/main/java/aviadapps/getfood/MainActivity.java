1f688271671e31824caa24e3720a9b95b4c82bbd
Aviad Katani <aviadkatani@gmail.com>
2017-01-02 10:38:37 +0200

    Add Fragment

--- app/src/main/java/aviadapps/getfood/MainActivity.java
+++ app/src/main/java/aviadapps/getfood/MainActivity.java
@@ -1,7 +1,11 @@
 package aviadapps.getfood;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void fragmentChange(View v) {
+        Fragment fragment = null;
+        if(v == findViewById(R.id.button)) {
+            fragment = new userFragment();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fragmentPlace, fragment);
+            ft.commit();
+
+        }
+        else if(v == findViewById(R.id.button2)) {
+            fragment = new companyFragment();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fragmentPlace, fragment);
+            ft.commit();
+        }
     }
 }
