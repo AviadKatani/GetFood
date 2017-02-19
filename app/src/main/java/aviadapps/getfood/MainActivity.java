3252ec1ff59b26899f6e34d46e4aa43c4bcea0d5
Aviad Katani <aviadkatani@gmail.com>
2017-02-19 12:38:41 +0200

    Design update

--- app/src/main/java/aviadapps/getfood/MainActivity.java
+++ app/src/main/java/aviadapps/getfood/MainActivity.java
@@ -7,17 +7,20 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textLog;
+    String userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void registerClicked(View view) {
-        Intent moveIntent = new Intent(this, LoginActivity.class);
-        startActivity(moveIntent);
+        textLog = (TextView) findViewById(R.id.textLog);
+        Intent getIntent = getIntent();
+        userName = getIntent.getStringExtra("Username");
+        textLog.setText("You're logged in, " + userName);
     }
 }
