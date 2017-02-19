f2882aaebeebfd6fb1414f5e1762e2b150b3714f
Aviad Katani <aviadkatani@gmail.com>
2017-02-19 23:20:44 +0200

    EditText validation add

--- app/src/main/java/aviadapps/getfood/RegisterActivity.java
+++ app/src/main/java/aviadapps/getfood/RegisterActivity.java
@@ -7,6 +7,8 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
     EditText userET, emailET, passwordET, phoneET, nameET;
@@ -26,10 +28,23 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     public void registerUser(View view) {
-        user = userET.getText().toString();
-        name = nameET.getText().toString();
-        email = emailET.getText().toString();
-        password = passwordET.getText().toString();
+        if(!isEmpty(userET))
+            user = userET.getText().toString();
+        else
+            userET.setError("Cannot be empty.");
+        if(!isEmpty(nameET))
+            name = nameET.getText().toString();
+        else
+            nameET.setError("Cannot be empty.");
+        if(isEmailValid(email))
+            email = emailET.getText().toString();
+        else
+            emailET.setError("Not a email address");
+        if(isValidPassword(password))
+            password = passwordET.getText().toString();
+        else
+            passwordET.setError("Pass should be more than 6 characters");
+
         phone = Integer.parseInt(phoneET.getText().toString());
         // TODO: Check the info provided by user
         HelperDB db = new HelperDB(this);
@@ -37,4 +52,20 @@ public class RegisterActivity extends AppCompatActivity {
         db.addUser(createUser);
         Toast.makeText(this, "Registered", Toast.LENGTH_LONG).show();
     }
+
+    boolean isEmailValid(CharSequence email) {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    private boolean isValidPassword(String pass) {
+        if (pass != null && pass.length() > 6) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isEmpty(EditText etText) {
+        return etText.getText().toString().trim().length() == 0;
+    }
+
 }
